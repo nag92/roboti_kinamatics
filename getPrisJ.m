070eb1624821e86d741117559b2fe1f8bfe2b5a2
ngoldfarb <nagoldfarb@wpi.edu>
2017-08-23 12:54:17 -0400

    adding functions for geting jacobian

---
+++ getPrisJ.m
@@ -0,0 +1,12 @@
+function [ j] = getPrisJ( z )
+%Nathaniel Goldfarb 
+%getRevJ returns the Jacobian of a prismatic joint
+%inputs:
+%z: 1X3 matrix
+%outputs:
+%j:1x4 jacobian matrix 
+
+j=[z;0;0;0];
+
+
+end
---
+++ getRevJ.m
@@ -0,0 +1,13 @@
+function [ j] = getRevJ( z,on,o )
+%Nathaniel Goldfarb 
+%getRevJ returns the Jacobian of a revolute joint
+%inputs:
+%z: 1X3 matrix
+%on: last joint 'o 'o' 1x3 matrix
+%o: current 'o' joint 1x3 matrix
+
+a= cross(z,(on-o));
+j= [a;z];
+
+
+end
