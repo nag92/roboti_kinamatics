dc90fc51311392e6b1a3e41e44ef81e509ef2b84
ngoldfarb <nagoldfarb@wpi.edu>
2017-08-25 22:53:24 -0400

    added function to get the Jacobain

---
+++ getJacobian.m
@@ -0,0 +1,14 @@
+function [ j ] = getJacobian( T )
+
+    n = length(T(1,1,:))
+    
+    [o,On] = getO(T);
+
+    z = getZ(T);
+    
+    for ii = 1:n
+        j(:,ii) = getRevJ(z(:,ii),On,o(:,ii));
+       
+
+end
+
